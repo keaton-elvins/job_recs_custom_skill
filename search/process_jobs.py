455f3a13ccdd6c87602457340d4b8872e6a6c2f9
keaton-elvins <keatonelvins@berkeley.edu>
2021-07-20 12:24:27 -0700

    job database updated

--- search/process_jobs.py
+++ search/process_jobs.py
@@ -5,22 +5,23 @@ from requests.api import get
 import spacy
 from spacy.pipeline import EntityRuler
 
-def get_skills(link=str):
+def get_skills(tag=str):
+    link = "https://www.indeed.com" + tag
     page = requests.get(link)
 
     soup = BeautifulSoup(page.content, "html.parser")
     description = soup.find(id='jobDescriptionText')
 
-    text_chunks = description.find_all(text=True)
-    
+    try:
+        text_chunks = description.find_all(text=True)
+    except AttributeError:
+        return {}
+
     text = " ".join(text_chunks).strip('\n')
-
     doc = ruler(nlp(text))
-
     skills = set([ent.label_[6:] for ent in doc.ents if ent.label_.startswith("SKILL|")])
 
     return skills
-
 
 nlp = spacy.load("en_core_web_sm")
 ruler = EntityRuler(nlp, overwrite_ents=True).from_disk("app/data/patterns.jsonl")
@@ -29,24 +30,29 @@ URL = "https://www.indeed.com/jobs?q=software+engineer&limit=50&start="
 
 jobs_list = []
 
-for i in range(0, 500, 50):
+for i in range(0, 1000, 50):
     page = requests.get(URL + str(i))
 
     soup = BeautifulSoup(page.content, "html.parser")
     job_soup = soup.find(id="mosaic-provider-jobcards")
 
-    job_results = job_soup.find_all('a', class_="tapItem")
+    try:
+        job_results = job_soup.find_all('a', class_="tapItem")
+    except:
+        continue
 
     for result in job_results:
-        link = "https://www.indeed.com" + result.get('href').strip()
+        link = result.get('href').strip()
         
         if not link.startswith("/pagead"):
-            job_entry = {}
-            job_entry.update({"Title": result.find("h2", class_='jobTitle').text.strip()})
-            job_entry.update({"Link": link})
-            job_entry.update({"Skills": get_skills(link)})
-            jobs_list.append(job_entry)
-                
+            skills = get_skills(link)
+            if skills:
+                job_entry = {}
+                job_entry.update({"Title": result.find("h2", class_='jobTitle').text.strip()})
+                job_entry.update({"Link": "https://www.indeed.com" + link})
+                job_entry.update({"Skills": skills})
+                jobs_list.append(job_entry)
+      
 
 csv_file = "search/Jobs.csv"
 try:
@@ -60,5 +66,6 @@ try:
                 continue
 except IOError:
     print("I/O error")
+
 
 
