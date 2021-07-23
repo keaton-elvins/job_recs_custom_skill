9575a97558d2a19f6449cb857dd29093b68f3060
keaton-elvins <keatonelvins@berkeley.edu>
2021-07-23 08:07:28 -0700

    venv update

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -1,2 +1,2 @@
 pip3 install --user virtualenv
-virtualenv .venv && source .venv/Scripts/activate && pip install -r ./requirements/base.txt && python -m spacy download en_core_web_sm
+virtualenv .venv && source .venv/bin/activate && pip install -r ./requirements/base.txt && python -m spacy download en_core_web_sm
