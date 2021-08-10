53e62d4ff52bff0657df0f3ae5ab83df60f288b3
keaton-elvins <keatonelvins@berkeley.edu>
2021-08-09 23:22:26 -0700

    reformat

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
 from collections import defaultdict
 import os
 
-from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI, Body
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
@@ -12,7 +8,7 @@ import spacy
 import srsly
 import pandas as pd
 
-from app.models import (
+from models import (
     ENT_PROP_MAP,
     RecordsRequest,
     RecordsResponse,
@@ -20,21 +16,16 @@ from app.models import (
 from spacy.pipeline import EntityRuler
 
 
-load_dotenv(find_dotenv())
-prefix = os.getenv("CLUSTER_ROUTE_PREFIX", "").rstrip("/")
-
-
 app = FastAPI(
     title="job_recs_custom_skill",
     version="1.0",
     description="Azure Search Cognitive Skill to recommend job openings based on extracted technical/business skills from text",
-    openapi_prefix=prefix,
 )
 
-example_request = srsly.read_json("app/data/example_request.json")
+example_request = srsly.read_json("data/example_request.json")
 
 nlp = spacy.load("en_core_web_sm")
-ruler = EntityRuler(nlp, overwrite_ents=True).from_disk("app/data/patterns.jsonl")
+ruler = EntityRuler(nlp, overwrite_ents=True).from_disk("data/patterns.jsonl")
 
 def get_score(skills, reqs):
     """Given a list of job requirements, evaluate score based on user's skills"""
@@ -48,7 +39,7 @@ def get_score(skills, reqs):
 
 @app.get("/", include_in_schema=False)
 def docs_redirect():
-    return RedirectResponse(f"{prefix}/docs")
+    return RedirectResponse("docs")
 
 
 @app.post("/recs", response_model=RecordsResponse, tags=["NER"])
@@ -63,7 +54,7 @@ async def find_recs(body: RecordsRequest = Body(..., example=example_request)):
     skills = set([ent.label_[6:] for ent in doc.ents if ent.label_.startswith("SKILL|")])
     job_recs = []
 
-    df = pd.read_csv("search/Jobs.csv", encoding='cp1252')
+    df = pd.read_csv("data/Jobs.csv", encoding='cp1252')
 
     # find top 10 jobs with highest score
     for i in df.index:
@@ -78,11 +69,12 @@ async def find_recs(body: RecordsRequest = Body(..., example=example_request)):
 
         elif score > weakest["Score"]:
             job_recs.remove(weakest)
-            weakest = min(job_recs, key=lambda rec: rec["Score"])
 
             row = df.loc[i]
-            job_recs.append({"Name":row[0], "Link":row[1], "Score":score})
-            
+            job_recs.append({"Name": row[0], "Link": row[1], "Score": score})
+
+            weakest = min(job_recs, key=lambda rec: rec["Score"])
+
     job_recs.reverse()
 
     res.update({"recordId": body.values[0].recordId})
--- create_virtualenv.sh
+++
@@ -1,2 +0,0 @@
-pip3 install --user virtualenv
-virtualenv .venv && source .venv/bin/activate && pip install -r ./requirements.txt && python -m spacy download en_core_web_sm
